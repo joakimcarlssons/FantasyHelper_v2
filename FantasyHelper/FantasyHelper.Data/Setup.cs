ef8a09571de27db91fc510d641faf13dcc73a333
Joakim <hej@joakimcarlsson.se>
2022-11-19 11:08:34 +0100

    Adding Data library

---
+++ FantasyHelper/FantasyHelper.Data/Setup.cs
@@ -0,0 +1,32 @@
+﻿using FantasyHelper.Data.Allsvenskan;
+using FantasyHelper.Data.FPL;
+using FantasyHelper.Shared.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FantasyHelper.Data
+{
+    public static class Setup
+    {
+        public delegate IRepository DbFactory(FantasyGames game);
+
+        public static IServiceCollection AddFantasyData(this IServiceCollection services)
+        {
+            services.AddScoped<FPLRepository>();
+            services.AddScoped<ASRepository>();
+            services.AddScoped<DbFactory>(serviceProvider => game =>
+            {
+                return game switch
+                {
+                    FantasyGames.Allsvenskan => serviceProvider.GetRequiredService<ASRepository>(),
+                    _ => serviceProvider.GetRequiredService<FPLRepository>()
+                };
+            });
+
+            services.AddDbContext<FPLDataContext>(opt => opt.UseInMemoryDatabase("InMemDb"));
+            services.AddDbContext<ASDataContext>(opt => opt.UseInMemoryDatabase("InMemDb"));
+
+            return services;
+        }
+    }
+}
